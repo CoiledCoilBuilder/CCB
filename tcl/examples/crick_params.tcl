4ee0f613fe53c9f9d80b372bae9baf601b9a33be
Chris MacDermaid <chris.macdermaid@gmail.com>
2014-02-24 16:29:58 -0500

    Updated license and header information

--- tcl/examples/crick_params.tcl
+++ tcl/examples/crick_params.tcl
@@ -1,27 +1,27 @@
-# -*- tcl -*-
+# -*-tcl-*-
 
-  # +------------------------------------------------------------------------------------+ 
-  # | This file is part of Coiled-Coil Builder.                                          | 
-  # |                                                                                    | 
-  # | Coiled-Coil Builder is free software: you can redistribute it and/or modify        | 
-  # | it under the terms of the GNU General Public License as published by               | 
-  # | the Free Software Foundation, either version 3 of the License, or                  | 
-  # | (at your option) any later version.                                                | 
-  # |                                                                                    | 
-  # | Coiled-Coil Builder is distributed in the hope that it will be useful,             | 
-  # | but WITHOUT ANY WARRANTY without even the implied warranty of                      | 
-  # | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                      | 
-  # | GNU General Public License for more details.                                       | 
-  # |                                                                                    | 
-  # | You should have received a copy of the GNU General Public License                  | 
-  # | along with Coiled-Coil Builder.  If not, see <http:www.gnu.org/licenses/>.         | 
-  # |                                                                                    | 
-  # |  *cr                                                                               | 
-  # |  *cr            (C) Copyright 1995-2013 The Board of Trustees of the               | 
-  # |  *cr                        University of Pennsylvania                             | 
-  # |  *cr                         All Rights Reserved                                   | 
-  # |  *cr                                                                               | 
-  # +------------------------------------------------------------------------------------+ 
+# *hd +------------------------------------------------------------------------------------+
+# *hd |  This file is part of Coiled-Coil Builder.                                         |
+# *hd |                                                                                    |
+# *hd |  Coiled-Coil Builder is free software: you can redistribute it and/or modify       |
+# *hd |  it under the terms of the GNU General Public License as published by              |
+# *hd |  the Free Software Foundation, either version 3 of the License, or                 |
+# *hd |  (at your option) any later version.                                               |
+# *hd |                                                                                    |
+# *hd |  Coiled-Coil Builder is distributed in the hope that it will be useful,            |
+# *hd |  but WITHOUT ANY WARRANTY without even the implied warranty of                     |
+# *hd |  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                     |
+# *hd |  GNU General Public License for more details.                                      |
+# *hd |                                                                                    |
+# *hd |  You should have received a copy of the GNU General Public License                 |
+# *hd |  along with Coiled-Coil Builder.  If not, see <http:www.gnu.org/licenses/>.        |
+# *hd +------------------------------------------------------------------------------------+
+
+# *hd | If you intend to use this software for your research, please cite:
+# *hd | and inform Chris MacDermaid <chris.macdermaid@gmail.com> of any pending publications.
+
+# *hd | Copyright (c) 2012,2013,2014 by Chris M. MacDermaid <chris.macdermaid@gmail.com>
+# *hd | and Jeffery G. Saven <saven@sas.upenn.edu>
 
 # Get the Crick Parameters for
 # a particular input structure
--- tcl/minimize/minimize.tcl
+++ tcl/minimize/minimize.tcl
@@ -1,11 +1,30 @@
-#!/usr/bin/tclsh
+# -*-tcl-*-
+
+# *hd +------------------------------------------------------------------------------------+
+# *hd |  This file is part of Coiled-Coil Builder.                                         |
+# *hd |                                                                                    |
+# *hd |  Coiled-Coil Builder is free software: you can redistribute it and/or modify       |
+# *hd |  it under the terms of the GNU General Public License as published by              |
+# *hd |  the Free Software Foundation, either version 3 of the License, or                 |
+# *hd |  (at your option) any later version.                                               |
+# *hd |                                                                                    |
+# *hd |  Coiled-Coil Builder is distributed in the hope that it will be useful,            |
+# *hd |  but WITHOUT ANY WARRANTY without even the implied warranty of                     |
+# *hd |  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                     |
+# *hd |  GNU General Public License for more details.                                      |
+# *hd |                                                                                    |
+# *hd |  You should have received a copy of the GNU General Public License                 |
+# *hd |  along with Coiled-Coil Builder.  If not, see <http:www.gnu.org/licenses/>.        |
+# *hd +------------------------------------------------------------------------------------+
+
+# *hd | If you intend to use this software for your research, please cite:
+# *hd | and inform Chris MacDermaid <chris.macdermaid@gmail.com> of any pending publications.
+
+# *hd | Copyright (c) 2012,2013,2014 by Chris M. MacDermaid <chris.macdermaid@gmail.com>
+# *hd | and Jeffery G. Saven <saven@sas.upenn.edu>
 
 ## Minimization Routines
 ## Numerical Recipies 2nd Ed.
-
-## Provided by Chris MacDermaid (Saven Group, University of Pennsylvania, MAY 2009)
-## Version 1.0
-## chris.macdermaid@gmail.com
 
 package provide minimize 1.0
 
